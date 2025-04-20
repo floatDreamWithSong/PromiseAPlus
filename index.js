ec71b2a1bb7390a2fb97d38d4df890b8e593704b
邓涵杰 <87105391+floatDreamWithSong@users.noreply.github.com>
2025-04-20 13:41:29 +0800

    init

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 module.exports = {
+    // 创建一个用value实现了的promise
     resolved: (value) => new PromiseAPlus(function (resolve) { resolve(value) }),
+    // 创建一个以reason拒绝了的promise
     rejected: (reason) => new PromiseAPlus(function (resolve, reject) { reject(reason) }),
-    deferred(){
+    deferred() {
         var resolve, reject
         return {
+            // 用value完成promise
             resolve: resolve,
+            //用于让promise变为拒绝
             reject: reject,
-            promise: new PromiseAPlus(function(res,rej){
-                resolve = res
+            // 一个正在处理中的promise
+            promise: new PromiseAPlus(function (res, rej) {
+                resolve = res+""
                 reject = rej
             })
         }
@@ -15,6 +20,26 @@ module.exports = {
 }
 
 class PromiseAPlus {
+    state
     constructor(callback) {
+        return this
+    }
+    /**
+     * @returns {PromiseAPlus}
+     */
+    then() {
+        return this
+    }
+    /**
+    * @returns {PromiseAPlus}
+    */
+    finally() {
+
+    }
+    /**
+    * @returns {PromiseAPlus}
+    */
+    catch() {
+
     }
 }
